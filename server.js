fbc267457b9e94dcf4025b438d91f9da2216c7be
Michael Bosworth <mtbosworth@gmail.com>
2013-10-29 22:32:02 -0500

    twitter integration

--- server.js
+++ server.js
@@ -2,13 +2,31 @@
 
 
 var Instagram = require('instagram-node-lib'),
+	Twit = require('twit'),
 	http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
 	port = process.env.PORT || 3000;
 
+	// Instagram
+	// CLIENT ID	6cb56ab8349f4f719e7865d0f6429946
+	// CLIENT SECRET	b0413f9089a749cc858d3187b026487a
+	// WEBSITE URL	https://www.balancedscale.com
+	// REDIRECT URI	http://www.balancedscale.com/instagram
+
+	// Twitter 
+	// Consumer key	faS1RUZsEwW9T7S4OM5BUw
+	// Consumer secret	dcT0wa1BCuxyg66QOb7HpMePKrX2KuRQtjPjTbjH4
+
 Instagram.set('client_id', '6cb56ab8349f4f719e7865d0f6429946');
 Instagram.set('client_secret', 'b0413f9089a749cc858d3187b026487a');
+
+var Twitter = new Twit({
+    consumer_key:         'faS1RUZsEwW9T7S4OM5BUw'
+  , consumer_secret:      'dcT0wa1BCuxyg66QOb7HpMePKrX2KuRQtjPjTbjH4'
+  , access_token:         '15933181-crJlx7JOjunRcthjxXtfRJW05e5ex3YjRI6SZmLd2'
+  , access_token_secret:  'hyvzzOpwBMmHnEy94qom5Sie3BHhEWXmcne3FQ8gT7sKB'
+});
 
 http.createServer(function (req, res) {
 
@@ -29,15 +47,35 @@ http.createServer(function (req, res) {
   		var now = Math.round((new Date()).getTime() / 1000);
   		var delta = 86400 * 2;
   		var later = now - delta;
+  		var finished = 0;
+  		var json = {};
 
+  		function respond() {
+  			finished++;
+  			if(finished == 2) {
+  				res.end(JSON.stringify(json));
+  			}
+  		}
+
+  		// Instagram search
 	  	Instagram.media.search({ 
 	  		lat: queryData.query.lat, 
 	  		lng: queryData.query.long, 
 	  		max_timestamp: now,
 	  		min_timestamp: later,
 	  		complete:function(data, pagination){
-	  		res.end(JSON.stringify(data));
-	  	}});
+	  			json.posts = data;
+	  			respond();
+  			}
+  		});
+
+		// Twitter search
+		Twitter.get('search/tweets', {q: 'geocode:'+queryData.query.lat + ',' + queryData.query.long + ',1km'}, function(err, reply) {
+			json.statuses = reply.statuses;
+  			respond();
+		});
+
+
 
 	// basic file server 
   	} else {
@@ -59,4 +97,4 @@ http.createServer(function (req, res) {
   	}
 
 }).listen(port);
-console.log("Solo Up!");
+console.log("Smells like updawg!");
